f2f3f2c25235f899ff9367a7df96d6828959140b
fraivone <francesco.ivone@cern.ch>
2022-03-01 17:52:43 +0100

    import etapartition boundaries from file

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ['ROOT_Utils','PFA_Analyzer_Utils','THR_Utils']
+__all__ = ['ROOT_Utils','PFA_Analyzer_Utils','THR_Utils','EtaPartitionBoundaries']
 import ROOT_Utils
 import PFA_Analyzer_Utils
 import THR_Utils
+import EtaPartitionBoundaries
