38aedb37060607a7b1146fdd6be1f4ae08791d17
Francesco Ivone <francesco.ivone@cern.ch>
2022-11-30 16:48:03 +0100

    Plot per superchamber

--- helperScript/HVScan.py
+++ helperScript/HVScan.py
@@ -13,11 +13,11 @@ ver=""
 
 if ver=="":
     hv_scan_output = "/eos/user/f/fivone/www/P5_Operations/Run3/RunMerge/HVScan/"
-    fa1 = ROOT.TF1("erf(x)","[2] + [2]*ROOT::Math::erf((x-[0])/(sqrt(2)*[1]))",0,700)
+    fa1 = ROOT.TF1("erf(x)","[2] + [2]*ROOT::Math::erf((x-[0])/(sqrt(2)*[1]))",0,720)
     
 if ver == "rev1":
     hv_scan_output = "/eos/user/f/fivone/www/P5_Operations/Run3/RunMerge/HVScan_rev1/"
-    fa1 = ROOT.TF1("erf(x)","2*[2]/(1 + exp( -(x-[0])/[1]  ) )",0,700)
+    fa1 = ROOT.TF1("erf(x)","2*[2]/(1 + exp( -(x-[0])/[1]  ) )",0,720)
 
 fa1.SetParNames("Mean","Sigma","Norm")
 fa1.SetParameters(650,30,50)
@@ -40,7 +40,7 @@ parser.add_argument('--batch', default=False, action='store_true',help="ROOT in 
 
 
 args = parser.parse_args()
-inputs = ["./Output/PFA_Analyzer_Output/CSV/"+i+"/" for i in args.inputs]
+inputs = [f"{OUTPUT_PATH}//PFA_Analyzer_Output/CSV/"+i+"/" for i in args.inputs]
 hv_list = [ k for k in args.HV ]
 
 if len(hv_list) != len(inputs):
@@ -52,7 +52,7 @@ if len(hv_list) != len(inputs):
 
 QC8_THR = []
 PRIMARY_XAXIS_MIN = 580
-PRIMARY_XAXIS_MAX = 700
+PRIMARY_XAXIS_MAX = 720
 PRIMARY_YAXIS_MIN = 0
 PRIMARY_YAXIS_MAX = 105
 
@@ -92,7 +92,7 @@ c2.SetTicky(0)
 c2.SetGrid()
 
 ## ROOT Objects
-TH1_OnsetDistr = ROOT.TH1F("mean Erd","mean Erf",40,0,0)
+TH1_OnsetDistr = ROOT.TH1F("mean Erd","mean Erf",100,550,850)
 TH1_chi2Distr = ROOT.TH1F("chi2 Fit","chi2 Fit",40,0,0)
 TH1_sigmaDistr = ROOT.TH1F("sigma Erf","sigma Erf",40,0,0)
 TH1_normDistr = ROOT.TH1F("norm Erf","norm Erf",40,0,0)
@@ -100,6 +100,7 @@ TH1_normDistr = ROOT.TH1F("norm Erf","norm Erf",40,0,0)
 TGraphError_Dict = {}
 Text_Dict = {}
 TGraph_THR_Dict = {}
+fit_results = {}
 
 for re,la in [(-1,1),(1,1),(-1,2),(1,2)]: 
     label = EndcapLayer2label(re,la)
@@ -108,9 +109,9 @@ for re,la in [(-1,1),(1,1),(-1,2),(1,2)]:
 ## Operative loop
 
 for re in [-1,1]:
-    for la in [1,2]:
-        label = EndcapLayer2label(re,la)           
-        for chamber in range(1,37):
+    for chamber in range(1,37):
+        for la in [1,2]:
+            label = EndcapLayer2label(re,la)           
             x,y,exl,exh,eyl,eyh = [],[],[],[],[],[]
             chamberID = getChamberName(re,chamber,la)
             muons_used,muons_matched = [],[]
@@ -149,6 +150,11 @@ for re in [-1,1]:
             TH1_chi2Distr.Fill(chi2)
             TH1_sigmaDistr.Fill(sigma)
             TH1_normDistr.Fill(norm*2)
+            fit_results[chamberID] = {
+                                    "mean":mean,
+                                    "sigma":sigma,
+                                    "norm":norm,
+                                    "chi2":chi2}
             
 
             TGraphError_Dict[label][chamberID].SetName(chamberID)
@@ -222,11 +228,79 @@ for re in [-1,1]:
             c2.Update()
 
             ## Output files
-            folder_name = hv_scan_output+label
+            folder_name = hv_scan_output + label
             CreatEOSFolder(folder_name)
             outputPath = folder_name+"/"+chamberID +".pdf"
             c2.SaveAs(outputPath)
             Convert2png(outputPath)
+
+        ## PLOT SUPERCHAMBER
+        ch1,l1 = getChamberName(re,chamber,1),EndcapLayer2label(re,1)
+        ch2,l2 = getChamberName(re,chamber,2),EndcapLayer2label(re,2)
+        if TGraphError_Dict[l1].get(ch1) == None or TGraphError_Dict[l2].get(ch2) == None:
+            continue
+        else:
+            TGraphError_Dict[l1][ch1].SetMarkerColor(ROOT.kBlue)
+            TGraphError_Dict[l1][ch1].SetLineColor(ROOT.kBlue)
+            TGraphError_Dict[l1][ch1].GetListOfFunctions().FindObject("erf(x)").SetLineColor(ROOT.kBlue)
+
+            TGraphError_Dict[l2][ch2].SetLineColor(ROOT.kRed)
+            TGraphError_Dict[l2][ch2].SetMarkerColor(ROOT.kRed)
+            TGraphError_Dict[l2][ch2].GetListOfFunctions().FindObject("erf(x)").SetLineColor(ROOT.kRed)
+
+            TGraphError_Dict[l1][ch1].Draw("APE")
+            TGraphError_Dict[l2][ch2].Draw("SAME PE")
+
+            latex = ROOT.TLatex()
+            latex.SetNDC()
+            latex.SetTextAngle(0)
+            latex.SetTextColor(ROOT.kBlack)
+
+            latex.SetTextAlign(12)
+            latex.SetTextSize(0.5*ROOT.gPad.GetTopMargin())
+            latex.SetTextFont(61)
+            latex.DrawLatex(ROOT.gPad.GetLeftMargin(), 1 - 0.65*ROOT.gPad.GetTopMargin(), "CMS")
+            latex.SetTextFont(52)
+            latex.SetTextSize(0.45*ROOT.gPad.GetTopMargin())
+            latex.DrawLatex(1.6*ROOT.gPad.GetLeftMargin(),1 - 0.74*ROOT.gPad.GetTopMargin(), "Preliminary")
+            latex.SetTextFont(42)
+
+            latex.SetTextSize(0.35*ROOT.gPad.GetTopMargin())
+            latex.DrawLatex(1.1*ROOT.gPad.GetLeftMargin(),1 - 1.2*c2.GetTopMargin(), "#color[4]{"+f"{ch1}"+"}")
+            latex.SetTextSize(0.3*ROOT.gPad.GetTopMargin())
+            latex.DrawLatex(1.1*ROOT.gPad.GetLeftMargin(),1 - 1.6*c2.GetTopMargin(), "#chi^{2}: "+f"{fit_results[ch1]['chi2']:1.3f}")
+            latex.DrawLatex(1.1*ROOT.gPad.GetLeftMargin(), 1 - 2*c2.GetTopMargin(), f"Mean: {fit_results[ch1]['mean']:3.2f} uA")
+            latex.DrawLatex(1.1*ROOT.gPad.GetLeftMargin(), 1 - 2.4*c2.GetTopMargin(), f"Std Dev: {fit_results[ch1]['sigma']:2.2f}")
+            latex.DrawLatex(1.1*ROOT.gPad.GetLeftMargin(), 1 - 2.8*c2.GetTopMargin(), f"Norm: {2*fit_results[ch1]['norm']:2.2f}")
+
+            latex.SetTextSize(0.35*ROOT.gPad.GetTopMargin())
+            latex.DrawLatex(1.1*ROOT.gPad.GetLeftMargin(),1 - 3.3*c2.GetTopMargin(), "#color[2]{"+f"{ch2}"+"}")
+            latex.SetTextSize(0.3*ROOT.gPad.GetTopMargin())
+            latex.DrawLatex(1.1*ROOT.gPad.GetLeftMargin(),1 - 3.7*c2.GetTopMargin(), "#chi^{2}: "+f"{fit_results[ch2]['chi2']:1.3f}")
+            latex.DrawLatex(1.1*ROOT.gPad.GetLeftMargin(), 1 - 4.1*c2.GetTopMargin(), f"Mean: {fit_results[ch2]['mean']:3.2f} uA")
+            latex.DrawLatex(1.1*ROOT.gPad.GetLeftMargin(), 1 - 4.5*c2.GetTopMargin(), f"Std Dev: {fit_results[ch2]['sigma']:2.2f}")
+            latex.DrawLatex(1.1*ROOT.gPad.GetLeftMargin(), 1 - 4.9*c2.GetTopMargin(), f"Norm: {2*fit_results[ch2]['norm']:2.2f}")
+
+
+            latex.SetTextAlign(32)
+            latex.SetTextSize(0.45*ROOT.gPad.GetTopMargin())
+            latex.DrawLatex(1-1.1*ROOT.gPad.GetRightMargin(),1 - 0.74*ROOT.gPad.GetTopMargin(), "#sqrt{s} = 13.6 TeV (2022)")
+
+
+            legend = ROOT.TLegend (0.6 ,0.2 ,0.9 ,0.4)
+            legend.AddEntry ( TGraphError_Dict[l1][ch1] , ch1, "pl" )
+            legend.AddEntry ( TGraphError_Dict[l2][ch2] , ch2, "pl" )
+            legend.SetLineWidth (0)
+            legend.Draw ("same") 
+            c2.Modified()
+            c2.Update()
+
+
+            file_name = f"{hv_scan_output}/GE{re}1_{chamber:02d}.pdf"
+            c2.SaveAs(file_name)
+            Convert2png(file_name)
+        ## PLOT SUPERCHAMBER
+            
 
 
 file_name = hv_scan_output+"HVOnsetDistribution.pdf"
